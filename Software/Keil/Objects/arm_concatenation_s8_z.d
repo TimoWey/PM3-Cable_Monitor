042b8d62424332588916de639bb9d44b7e96d752
Timää <49094206+TimoWey@users.noreply.github.com>
2023-10-13 11:09:36 +0200

    software gugus

---
+++ Software/Keil/Objects/arm_concatenation_s8_z.d
@@ -0,0 +1,16 @@
+./objects/arm_concatenation_s8_z.o: \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS-NN\4.0.0\Source\ConcatenationFunctions\arm_concatenation_s8_z.c \
+  RTE\_Target_1\Pre_Include_Global.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS-NN\4.0.0\Include\arm_nnfunctions.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS-NN\4.0.0\Include\arm_nn_math_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\float.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\limits.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS-NN\4.0.0\Include\arm_nn_types.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS-NN\4.0.0\Include\arm_nnsupportfunctions.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h
