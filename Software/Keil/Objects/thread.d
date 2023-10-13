2519f5bf875a324f7b0e67d367b1a2ee0a40d37a
Timää <timo@ritawey.ch>
2023-10-13 11:09:36 +0200

    software gugus

---
+++ Software/Keil/Objects/thread.d
@@ -0,0 +1,5 @@
+./objects/thread.o: Thread.c RTE\_Target_1\Pre_Include_Global.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\RTOS\RTX\INC\cmsis_os.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Users\weyti\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.16.0\Board\Board_LED.h
