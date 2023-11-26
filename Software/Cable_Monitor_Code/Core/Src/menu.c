0052e392633a5976931695c3e78e82edbfe71950
Timaeae <49094206+TimoWey@users.noreply.github.com>
2023-11-26 11:32:31 +0100

    somewhat working state machine(GUI)

--- Software/Cable_Monitor_Code/Core/Src/menu.c
+++ Software/Cable_Monitor_Code/Core/Src/menu.c
@@ -33,46 +33,88 @@
 /******************************************************************************
  * Defines
  *****************************************************************************/
-#define MENU_FONT				&Font12	///< Possible font sizes: 8 12 16 20 24
-#define MENU_HEIGHT				40		///< Height of menu bar
-#define MENU_MARGIN				2		///< Margin around a menu entry
+#define MENU_NUM_OF_DIFF_MENU	6 ///< Number of different menu pages
+#define MENU_NUM_OF_MENU_ELEM	3 ///< Number of menu elements on the page
+#define MENU_ENTRY_COUNT		MENU_NUM_OF_DIFF_MENU*MENU_NUM_OF_MENU_ELEM
+								  ///< Number of menu entries
+#define MENU_FONT				&Font12 ///< Possible font sizes: 8 12 16 20 24
+#define MENU_HEIGHT				40 ///< Height of menu bar
+#define MENU_MARGIN				2 ///< Margin around a menu entry
 /** Position of menu bar: 0 = top, (BSP_LCD_GetYSize()-MENU_HEIGHT) = bottom */
 #define MENU_Y					(BSP_LCD_GetYSize()-MENU_HEIGHT)
 
+
+/******************************************************************************
+ * Types
+ *****************************************************************************/
+/** Enumeration of possible menu items */
+typedef enum {
+	MENU_SING = 0,	///< Single measurement button
+	MENU_ACCU,		///< Accurate measurement button
+	MENU_CALB,		///< Calibration button
+
+	MENU_S_1P,		///< Single measurement one phase button
+	MENU_S_HO,		///< Single measurement home button
+	MENU_S_3P,		///< Single measurement three phase button
+
+	MENU_A_1P,		///< Accurate measurement one phase button
+	MENU_A_HO,		///< Accurate measurement home button
+	MENU_A_3P,		///< Accurate measurement three phase button
+
+	MENU_C_CI,		///< Calibration current button
+	MENU_C_HO,		///< Calibration home button
+	MENU_C_PD,		///< Calibration distance button
+
+	MENU_C_1P,		///< Calibration current one phase button
+	MENU_C_AB,		///< Calibration current aboard button
+	MENU_C_3P,		///< Calibration current three phase button
+
+	MENU_P_NE,		///< Calibration distance next button
+	MENU_P_AB,		///< Calibration distance aboard button
+	MENU_P_BA,		///< Calibration distance back button
+
+	MENU_NONE		///< Do nothing
+} MENU_item_t;
 
 /******************************************************************************
  * Variables
  *****************************************************************************/
 static MENU_item_t MENU_transition = MENU_NONE;	///< Transition to this menu
 
-static MENU_entry_t MENU_entry[MENU_ENTRY_COUNT] = {
-		{"Measurement",	"-",			LCD_COLOR_BLACK,	LCD_COLOR_LIGHTBLUE},
-		{"Calibration",	"-",			LCD_COLOR_BLACK,	LCD_COLOR_LIGHTGREEN},
-		{"Info",	    "-",			LCD_COLOR_BLACK,	LCD_COLOR_LIGHTRED},
+static MENU_entry_t MENU_entry[MENU_ENTRY_COUNT] =
+{
+	{"single",	"measurem.",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTCYAN},
+	{"accurate","measurem.",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTGREEN},
+	{"calibra.","screen",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTMAGENTA},
 
-		//MEASUREMENTS
-		{"Single",		"Measurement",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTBLUE},
-		{"Accurate",	"Measurement",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTBLUE},
-		{"Return",		"to Home",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTRED},
+	{"1.phase",	"measurem.",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTCYAN},
+	{"home",	"screen",		LCD_COLOR_WHITE,	LCD_COLOR_GRAY},
+	{"3.phase",	"measurem.",	LCD_COLOR_WHITE,	LCD_COLOR_DARKCYAN},
 
-		//SINGLE
-		{"Single",		"Measurement",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTBLUE},
-		{"Return",		"to Home",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTRED},
+	{"1.phase",	"measurem.",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTGREEN},
+	{"home",	"screen",		LCD_COLOR_WHITE,	LCD_COLOR_GRAY},
+	{"3.phase",	"measurem.",	LCD_COLOR_WHITE,	LCD_COLOR_DARKGREEN},
 
-		//ACCURATE
-		{"Accurate",	"Measurement",	LCD_COLOR_BLACK,	LCD_COLOR_LIGHTBLUE},
-		{"Return",		"to Home",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTRED},
+	{"calibra.","current",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTRED},
+	{"home",	"screen",		LCD_COLOR_WHITE,	LCD_COLOR_GRAY},
+	{"calibra.","distance",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTYELLOW},
 
-		//CALIBRATION
-		{"Calibrate",	"-",			LCD_COLOR_BLACK,	LCD_COLOR_LIGHTGREEN},
-		{"Return",		"to Home",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTRED},
-};		///< All the menu entries
+	{"1.phase",	"current",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTRED},
+	{"abort",	"current",		LCD_COLOR_WHITE,	LCD_COLOR_GRAY},
+	{"3.phase", "current",		LCD_COLOR_WHITE,	LCD_COLOR_BROWN},
+
+	{"next",	"distance",		LCD_COLOR_BLACK,	LCD_COLOR_LIGHTYELLOW},
+	{"abort",	"distance",		LCD_COLOR_WHITE,	LCD_COLOR_GRAY},
+	{"back",	"distance",		LCD_COLOR_WHITE,	LCD_COLOR_ORANGE},
+};		///< first block home menu, second meas single, third meas accurate,
+		///< fourth calibration, fifth cali current, sixth cali distance		///< All the menu entries
 
 
 /******************************************************************************
  * Functions
  *****************************************************************************/
-
+static void MENU_check_transition(MENU_type_t type);
+static MENU_item_t MENU_get_transition(void);
 
 /** ***************************************************************************
  * @brief Handles the main menu state machine.
@@ -84,71 +126,142 @@ static MENU_entry_t MENU_entry[MENU_ENTRY_COUNT] = {
 void MENU_main(void)
 {
 	//static CALC_meas_data_t meas_data;
-	static MENU_item_t menu = MENU_HOME;
+	static MENU_type_t menu = MENU_HOME;
 
-	/* Comment next line if touchscreen interrupt is enabled */
 	MENU_check_transition(menu);
 
-	switch (MENU_get_transition())
-	{	// Handle user menu choice
-		case MENU_HOME:
-            // Handle home menu selection
-			MENU_hint();
-			MENU_draw(0, 3);
-            break;
-        case MENU_MEASUREMENTS:
-			MENU_Measurements();
-        	MENU_draw(4, 7);
-            // Handle measurements menu selection
-            break;
-        case MENU_SINGLE:
-            // Handle single menu selection
-            break;
-        case MENU_ACCURATE:
-            // Handle accurate menu selection
-            break;
-        case MENU_CALIBRATION:
-			MENU_Calibrations();
-			MENU_draw(10, 11);
-            // Handle calibration menu selection
-            break;
-        case MENU_INFO:
-			MENU_Info();
-            // Handle info menu selection
-            break;
-		case MENU_RETURN:
-			menu = MENU_HOME;
-			break;
-        case MENU_NONE:
-            // Handle none menu selection
-            break;
-        default:
-            // Handle invalid menu selection
-            break;
-		/*case MENU_ZERO:
-			ADC3_IN4_single_init();
-			ADC3_IN4_single_read();
-			break;
-		case MENU_ONE:
-			ADC3_IN4_timer_init();
-			ADC3_IN4_timer_start();
-			break;
-		case MENU_TWO:
-			ADC3_IN4_DMA_init();
-			ADC3_IN4_DMA_start();
-			break;
-		case MENU_THREE:
-			ADC1_IN13_ADC2_IN5_dual_init();
-			ADC1_IN13_ADC2_IN5_dual_start();
-			break;
-		case MENU_FOUR:
-			ADC2_IN13_IN5_scan_init();
-			ADC2_IN13_IN5_scan_start();
-			break;
-		case MENU_FIVE:
-			ADC3_IN13_IN4_scan_init();
-			ADC3_IN13_IN4_scan_start();
-			break;*/
+	switch (MENU_get_transition()) {	// Handle user menu choice
+	case MENU_NONE:						// No transition => do nothing
+		break;
+
+	case MENU_SING:
+		menu = MENU_M_SI;
+		DISP_info_screen(menu);			// Show single meas. menu
+		break;
+	case MENU_ACCU:
+		menu = MENU_M_AC;
+		DISP_info_screen(menu);			// Show accurate meas. menu
+		break;
+	case MENU_CALB:
+		menu = MENU_CALI;
+		DISP_info_screen(menu);			// Show calibration menu
+		break;
+
+	case MENU_S_1P:
+		//CALC_start_single_measurement(&meas_data, CALC_ONE_PHASE);
+		//DISP_value_screen_sing(meas_data, CALC_ONE_PHASE);
+		break;
+	case MENU_S_3P:
+		//CALC_start_single_measurement(&meas_data, CALC_THREE_PHASE);
+		//DISP_value_screen_sing(meas_data, CALC_THREE_PHASE);
+		break;
+
+	case MENU_A_1P:
+		//CALC_start_accurate_measurement(&meas_data, CALC_ONE_PHASE);
+		//DISP_value_screen_accu(meas_data, CALC_ONE_PHASE);
+		break;
+	case MENU_A_3P:
+		//CALC_start_accurate_measurement(&meas_data, CALC_THREE_PHASE);
+		//DISP_value_screen_accu(meas_data, CALC_THREE_PHASE);
+		break;
+
+	case MENU_C_PD:
+		menu = MENU_C_PA;
+		DISP_info_screen(menu);			// Show distance calibration menu
+		break;
+	case MENU_C_CI:
+		menu = MENU_C_CO;
+		DISP_info_screen(menu);			// Show current calibration menu
+		break;
+
+	case MENU_C_1P:
+		/*if (MENU_coil_cali(MENU_C_1P) >= CALC_CALI_C_END) {
+			menu = MENU_CALI;
+			DISP_info_screen(menu);		// Show calibration menu
+		}*/
+		break;
+	case MENU_C_AB:
+		/*if (MENU_coil_cali(MENU_C_AB) >= CALC_CALI_C_END) {
+			menu = MENU_CALI;
+			DISP_info_screen(menu);		// Show calibration menu
+		}*/
+		break;
+	case MENU_C_3P:
+		/*if (MENU_coil_cali(MENU_C_3P) >= CALC_CALI_C_END) {
+			menu = MENU_CALI;
+			DISP_info_screen(menu);		// Show calibration menu
+		}*/
+		break;
+
+	case MENU_P_NE:
+		/*if (MENU_pad_cali(MENU_P_NE) >= CALC_CALI_P_END) {
+			menu = MENU_CALI;
+			DISP_info_screen(menu);		// Show calibration menu
+		}*/
+		break;
+	case MENU_P_AB:
+		/*if (MENU_pad_cali(MENU_P_AB) >= CALC_CALI_P_END) {
+			menu = MENU_CALI;
+			DISP_info_screen(menu);		// Show calibration menu
+		}*/
+		break;
+	case MENU_P_BA:
+		/*if (MENU_pad_cali(MENU_P_BA) >= CALC_CALI_P_END) {
+			menu = MENU_CALI;
+			DISP_info_screen(menu);		// Show calibration menu
+		}*/
+		break;
+
+	case MENU_C_HO:
+	case MENU_A_HO:
+	case MENU_S_HO:
+		menu = MENU_HOME;
+		DISP_info_screen(menu);			// Show home menu
+		break;
+	default:							// Should never occur
+		break;
+	}
+}
+
+/** ***************************************************************************
+ * @brief Displays the info screen
+ * @param[in] type	Actual menu type
+ *
+ * Displays the title and information on each screen.
+ *****************************************************************************/
+void DISP_info_screen(MENU_type_t type)
+{
+	BSP_LCD_Clear(LCD_COLOR_WHITE);		//Clear display
+	MENU_draw(type);					//Draw the home menu
+
+	switch (type) {
+	case MENU_HOME:
+		/* Show info home */
+		MENU_hint();
+		break;
+	case MENU_M_SI:
+		/* Show info single meas. */
+		//DISP_info_measurement((uint8_t *)"Single Meas.");
+		break;
+	case MENU_M_AC:
+		/* Show info accurate meas. */
+		//DISP_info_measurement((uint8_t *)"Accurate Meas.");
+		break;
+	case MENU_CALI:
+		/* Show info calibration */
+		//DISP_info_calibration();
+		break;
+	case MENU_C_CO:
+		/* Show coil info calibration */
+		//DISP_info_cali_coil();
+		break;
+	case MENU_C_PA:
+		/* Show pad info calibration */
+		//DISP_info_cali_pad();
+		break;
+	default:
+		/* Should never occur */
+		break;
 	}
 }
 
@@ -161,32 +274,30 @@ void MENU_main(void)
  * Text and background colors are applied.
  * @n These attributes are defined in the variable MENU_draw[].
  *****************************************************************************/
-void MENU_draw(uint8_t start_item, uint8_t end_item)
+void MENU_draw(MENU_type_t type)
 {
-	uint8_t count = end_item - start_item;
-	
 	BSP_LCD_SetFont(MENU_FONT);
-	uint32_t x, y, m, w, h;
+	uint32_t x, y, m, w, h, offset;
 	y = MENU_Y;
 	m = MENU_MARGIN;
-	w = BSP_LCD_GetXSize()/count;
+	w = BSP_LCD_GetXSize()/MENU_NUM_OF_MENU_ELEM;
 	h = MENU_HEIGHT;
-	
-	
-	for (uint8_t i = start_item; i < end_item; i++) 
-	{
+	offset = 0;
+	if (type < MENU_NUM_OF_DIFF_MENU) {
+		offset = type*MENU_NUM_OF_MENU_ELEM;	//calculate the menu
+	}
+	for (uint32_t i = 0; i < MENU_NUM_OF_MENU_ELEM; i++) {
 		x = i*w;
-		BSP_LCD_SetTextColor(MENU_entry[i].back_color);
+		BSP_LCD_SetTextColor(MENU_entry[i+offset].back_color);
 		BSP_LCD_FillRect(x+m, y+m, w-2*m, h-2*m);
-		BSP_LCD_SetBackColor(MENU_entry[i].back_color);
-		BSP_LCD_SetTextColor(MENU_entry[i].text_color);
+		BSP_LCD_SetBackColor(MENU_entry[i+offset].back_color);
+		BSP_LCD_SetTextColor(MENU_entry[i+offset].text_color);
 		BSP_LCD_DisplayStringAt(x+3*m, y+3*m,
-				(uint8_t *)MENU_entry[i].line1, LEFT_MODE);
+				(uint8_t *)MENU_entry[i+offset].line1, LEFT_MODE);
 		BSP_LCD_DisplayStringAt(x+3*m, y+h/2,
-				(uint8_t *)MENU_entry[i].line2, LEFT_MODE);
+				(uint8_t *)MENU_entry[i+offset].line2, LEFT_MODE);
 	}
 }
-
 
 /** ***************************************************************************
  * @brief Shows a hint at startup.
@@ -194,7 +305,7 @@ void MENU_draw(uint8_t start_item, uint8_t end_item)
  *****************************************************************************/
 void MENU_hint(void)
 {
-	BSP_LCD_Clear(LCD_COLOR_WHITE);
+	//BSP_LCD_Clear(LCD_COLOR_WHITE);
 	BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
 	BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
 	BSP_LCD_SetFont(&Font24);
@@ -322,48 +433,28 @@ MENU_item_t MENU_get_transition(void)
  * in the touch controller compared to the display.
  * Uncomment or comment the <b>\#define EVAL_REV_E</b> in main.h accordingly.
  *****************************************************************************/
-void MENU_check_transition(MENU_item_t menu)
+static void MENU_check_transition(MENU_type_t type)
 {
-
 	static MENU_item_t item_old = MENU_NONE;
 	static MENU_item_t item_new = MENU_NONE;
 	static TS_StateTypeDef  TS_State;	// State of the touch controller
-	uint32_t menu_offset = 0;
+	uint32_t offset = 0;
 	BSP_TS_GetState(&TS_State);			// Get the state
-
-
-// Evalboard revision E (blue) has an inverted y-axis in the touch controller
 #ifdef EVAL_REV_E
+/* Evalboard revision E (blue) has an inverted y-axis in the touch controller */
 	TS_State.Y = BSP_LCD_GetYSize() - TS_State.Y;	// Invert the y-axis
 #endif
-	// Invert x- and y-axis if LCD ist flipped
-#ifdef FLIPPED_LCD
-	TS_State.X = BSP_LCD_GetXSize() - TS_State.X;	// Invert the x-axis
-	TS_State.Y = BSP_LCD_GetYSize() - TS_State.Y;	// Invert the y-axis
-#endif
-
-
-/*
-	#if (defined(EVAL_REV_E) && !defined(FLIPPED_LCD)) || (!defined(EVAL_REV_E) && defined(FLIPPED_LCD))
-	TS_State.Y = BSP_LCD_GetYSize() - TS_State.Y;	// Invert the y-axis
-#endif
-#ifdef EVAL_REV_E
-#endif
-*/
-
 	if (TS_State.TouchDetected) {		// If a touch was detected
 		/* Do only if last transition not pending anymore */
 		if (MENU_NONE == MENU_transition) {
 			item_old = item_new;		// Store old item
 			/* If touched within the menu bar? */
 			if ((MENU_Y < TS_State.Y) && (MENU_Y+MENU_HEIGHT > TS_State.Y)) {
-				//Calculate which menu is next
-				if(menu < MENU_NUM_NUM_ENTRIES) {
-					menu_offset = menu*MENU_NUM_MENU_ELEMENTS; //calculate the menu
+				if(type < MENU_NUM_OF_DIFF_MENU) {
+					offset = type*MENU_NUM_OF_MENU_ELEM; //calculate the menu
 				}
-				
-				item_new = TS_State.X	// Calculate new item
-						/ (BSP_LCD_GetXSize()/MENU_NUM_MENU_ELEMENTS);
+				item_new = (TS_State.X	// Calculate new item
+						/ (BSP_LCD_GetXSize()/MENU_NUM_OF_MENU_ELEM)) + offset;
 				if ((0 > item_new) || (MENU_ENTRY_COUNT <= item_new)) {
 					item_new = MENU_NONE;	// Out of bounds
 				}
