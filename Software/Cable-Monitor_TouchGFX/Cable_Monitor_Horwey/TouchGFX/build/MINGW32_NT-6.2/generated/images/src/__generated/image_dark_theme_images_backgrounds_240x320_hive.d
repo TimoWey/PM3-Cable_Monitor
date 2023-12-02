8b3b47be80e61490a0867d67a6600db631791c64
Timaeae <timo@ritawey.ch>
2023-12-02 21:17:36 +0100

    pins for on off defined in mx

--- Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__generated/image_dark_theme_images_backgrounds_240x320_hive.d
+++
@@ -1,5 +0,0 @@
-build/MINGW32_NT-6.2/generated/images/src/__generated/image_dark_theme_images_backgrounds_240x320_hive.o: \
- generated/images/src/__generated/image_dark_theme_images_backgrounds_240x320_hive.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
