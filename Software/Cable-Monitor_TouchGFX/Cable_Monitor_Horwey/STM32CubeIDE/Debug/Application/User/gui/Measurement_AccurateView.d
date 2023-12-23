d5c335559a9c8ac6018fb9511ea49ce90160b3e1
Timää <49094206+TimoWey@users.noreply.github.com>
2023-12-23 11:47:38 +0100

    flash erweitert

--- Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/STM32CubeIDE/Debug/Application/User/gui/Measurement_AccurateView.d
+++ Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/STM32CubeIDE/Debug/Application/User/gui/Measurement_AccurateView.d
@@ -1,5 +1,5 @@
 Application/User/gui/Measurement_AccurateView.o: \
- C:/Users/aleja/Documents/Github/PM3-Cable_Monitor/Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/TouchGFX/gui/src/measurement_accurate_screen/Measurement_AccurateView.cpp \
+ C:/Github/Cable-Monitor/PM3-Cable_Monitor/Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/TouchGFX/gui/src/measurement_accurate_screen/Measurement_AccurateView.cpp \
  ../../TouchGFX/gui/include/gui/measurement_accurate_screen/Measurement_AccurateView.hpp \
  ../../TouchGFX/generated/gui_generated/include/gui_generated/measurement_accurate_screen/Measurement_AccurateViewBase.hpp \
  ../../TouchGFX/gui/include/gui/common/FrontendApplication.hpp \
