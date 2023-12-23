392e62664c46f40f5990421cf87829dc5855efc1
Timää <49094206+TimoWey@users.noreply.github.com>
2023-12-23 11:54:07 +0100

    load transition fixed

--- Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/STM32CubeIDE/Debug/Application/User/generated/image_alternate_theme_images_widgets_gauge_small_needles_rough.d
+++
@@ -1,4 +0,0 @@
-Application/User/generated/image_alternate_theme_images_widgets_gauge_small_needles_rough.o: \
- C:/Users/aleja/Documents/Github/PM3-Cable_Monitor/Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/TouchGFX/generated/images/src/__generated/image_alternate_theme_images_widgets_gauge_small_needles_rough.cpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
