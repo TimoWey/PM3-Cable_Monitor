937670214e2d474b49768d4b3884a45778e13bd9
Timää <timo@ritawey.ch>
2023-12-23 11:47:38 +0100

    flash erweitert

--- Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/STM32CubeIDE/Debug/Application/User/generated/image_checkmark.d
+++
@@ -1,4 +0,0 @@
-Application/User/generated/image_checkmark.o: \
- C:/Users/aleja/Documents/Github/PM3-Cable_Monitor/Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/TouchGFX/generated/images/src/image_checkmark.cpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
