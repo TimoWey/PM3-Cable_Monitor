c8ff26f3ed6fd78630702b202616f98eaf7acf7c
Timaeae <49094206+TimoWey@users.noreply.github.com>
2023-12-27 13:46:02 +0100

    Merge branch 'Merge_Final' of https://github.com/TimoWey/PM3-Cable_Monitor into Merge_Final

--- Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/Doxygen/Doxygen/html/search/functions_9.js
+++ Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/Doxygen/Doxygen/html/search/functions_9.js
@@ -4,5 +4,6 @@ var searchData=
   ['lcd_5fio_5finit_1',['LCD_IO_Init',['../main_8c.html#accf0a45e037a886f9b1a849bedd63735',1,'main.c']]],
   ['lcd_5fio_5freaddata_2',['LCD_IO_ReadData',['../main_8c.html#a10fd152b0ec2326173d80e922d8635fc',1,'main.c']]],
   ['lcd_5fio_5fwritedata_3',['LCD_IO_WriteData',['../main_8c.html#aae6ab499c7ca32f026708cbfb5153c97',1,'main.c']]],
-  ['lcd_5fio_5fwritereg_4',['LCD_IO_WriteReg',['../main_8c.html#a435f47c3f7c327325ea17f71fae7cebc',1,'main.c']]]
+  ['lcd_5fio_5fwritereg_4',['LCD_IO_WriteReg',['../main_8c.html#a435f47c3f7c327325ea17f71fae7cebc',1,'main.c']]],
+  ['load_5fflash_5fvalues_5',['Load_FLASH_Values',['../class_calibration_view.html#a0b00300d1f4217da9c77355b28377cf5',1,'CalibrationView']]]
 ];
--- Software/Cable-Monitor_TouchGFX/Cable_Monitor_Horwey/Doxygen/Doxygen/html_/search/variables_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['frequency_0',['frequency',['../struct_s_i_n_g_l_e___m_e_a_s.html#a94e2b46dcf9e62c53d6babc26087deeb',1,'SINGLE_MEAS::frequency'],['../struct_a_c_c_u___m_e_a_s.html#a94e2b46dcf9e62c53d6babc26087deeb',1,'ACCU_MEAS::frequency']]],
-  ['frequency_5fstd_5fdev_1',['frequency_std_dev',['../struct_a_c_c_u___m_e_a_s.html#a6623774f09a99cfdfbdbb81e628b1ad6',1,'ACCU_MEAS']]]
-];
